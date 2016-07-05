ef5c5a6fe851068a220b2ce1585483086970a539
erogetan <erogetan@gmail.com>
2016-07-05 13:22:28 -0400

    Update dump.sql

--- mysql/dump.sql
+++ mysql/dump.sql
@@ -7,6 +7,17 @@ state char(2),
 zipcode char(5)
 );
 
+CREATE TABLE a(
+
+  `id` int(255) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `Fav_City` varchar(255) NOT NULL,
+  `Emp_ID` varchar(255) NOT NULL,
+  `Name_Of_Company` varchar(255) NOT NULL
+
+);
+
 
 INSERT INTO `address`(`id`, `address`, `city`, `state`, `zipcode`) VALUES ('1','8042 Pleasant Point Lane','Myrtle Beach','SC','29579');
 

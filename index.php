710180e3f72daea8fb3512ea8db6f96d9c9c0e37
mannionjm <mannionjm@gmail.com>
2016-07-05 14:32:47 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -8,25 +8,15 @@
 	{
 		//This code will place the entered data into an
 		//Associative array after cleansing
-		$formfield['efirstname'] = trim($_POST['employeefirstname']);
-		$formfield['elastname'] = trim($_POST['employeelastname']);
-		$formfield['eaddress'] = trim($_POST['adress']);
+
+		$formfield['eaddress'] = trim($_POST['address']);
 		$formfield['ecity'] = trim($_POST['city']);
 		$formfield['estate'] = trim($_POST['state']);
 		$formfield['ezip'] = trim($_POST['zip']);
-		$formfield['ephone'] = trim($_POST['employeephone']);
 		
 		
 		//Validates that the fields were entered
-		if(empty($formfield['efirstname'])) {
-			$errormsg .= "<p>Your first name is empty</p>";
-		}
-		if(empty($formfield['elastname'])) {
-			$errormsg .= "<p>Your last name is empty</p>";
-		}
-		if(empty($formfield['eemail'])) {
-			$errormsg .= "<p>Your E-Mail is empty</p>";
-		}
+
 		if(empty($formfield['eaddress'])) {
 			$errormsg .= "<p>Your address is empty</p>";
 		}
@@ -36,9 +26,7 @@
 		if(empty($formfield['estate'])) {
 			$errormsg .= "<p>Your state empty</p>";
 		}
-		if(empty($formfield['ephone'])) {
-			$errormsg .= "<p>Your phone number is empty</p>";
-		}
+
 		//This could check two fields to see if they were the same
 		//Good for passwords and emails
 		/*
@@ -53,28 +41,26 @@
 		}		
 			//echo $securepwd;
 			//Creates the sql query
-			$sqlinsert = 'INSERT INTO address (empfirstname,
-				empmiddlename, emplastname, address, city, state, zip, phone) 
-				VALUES (:thefname, :themname, :thelname, :theaddress, :thecity, :thestate, :thezip, :thephone)';
+			$sqlinsert = 'INSERT INTO address (address, city, state, zipcode) 
+				VALUES (:theaddress, :thecity, :thestate, :thezip)';
 			
 			//Prepares the SQL Statement for execution
 			$stmtinsert = $db->prepare($sqlinsert);
 			//Binds our associative array variables to the bound
 			//variables in the sql statement
-			$stmtinsert->bindvalue(':thefname', $formfield['efirstname']);
-			$stmtinsert->bindvalue(':thelname', $formfield['elastname']);
+
 			$stmtinsert->bindvalue(':theaddress', $formfield['eaddress']);
 			$stmtinsert->bindvalue(':thecity', $formfield['ecity']);
 			$stmtinsert->bindvalue(':thestate', $formfield['estate']);
 			$stmtinsert->bindvalue(':thezip', $formfield['ezip']);
-			$stmtinsert->bindvalue(':thephone', $formfield['ephone']);
+
 			//Runs the insert statement and query
 			$stmtinsert->execute();
 			
 		}
 		
 		
-	}
+	
 	
 	
 	$sqlselect = "SELECT * from address";
@@ -89,16 +75,7 @@
 		<div>
 		<table>
 
-			<tr>
-				<th>First Name:</th>
-				<td><input type="text" name="employeefirstname" id="employeefirstname" 
-					value = "<?php echo $formfield['efirstname'] ?>" ></td>
-			</tr>
-			<tr>
-				<th>Last Name:</th>
-				<td><input type="text" name="employeelastname" id="employeelastname" 
-					value = "<?php echo $formfield['elastname'] ?>" ></td>
-			</tr>
+
 			<tr>
 				<th>Address:</th>
 				<td><input type="text" name="address" id="address" 
@@ -119,11 +96,7 @@
 				<td><input type="text" name="zip" id="zip" 
 					value = "<?php echo $formfield['ezip'] ?>" ></td>
 			</tr>				
-			<tr>
-				<th>Phone Number:</th>
-				<td><input type="text" name="phonenumber" id="phonenumber" 
-					value = "<?php echo $formfield['ephone'] ?>" ></td>
-			</tr>
+
 	
 		</table>
 		</div>

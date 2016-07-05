70bb983090de2354e3cf094a91a4dfc9dd8fa0c8
mannionjm <mannionjm@gmail.com>
2016-07-05 13:08:11 -0400

    Add files via upload

--- mysql/dump.sql
+++ mysql/dump.sql
@@ -1,1 +1,11 @@
-dump.sql
+CREATE TABLE address
+(
+id int(10),
+address char(75),
+city char(50),
+state char(2),
+zipcode char(5)
+);
+
+
+INSERT INTO `address`(`id`, `address`, `city`, `state`, `zipcode`) VALUES ('1','8042 Pleasant Point Lane','Myrtle Beach','SC','29579’);

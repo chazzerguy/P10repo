a2476b9c0fc63f3cd60de88eea3a5b5db71be250
Tmcgill2014 <tameramcgill1@gmail.com>
2016-07-05 13:21:20 -0400

    Add files via upload

--- mysql/dump.sql
+++ mysql/dump.sql
@@ -8,4 +8,12 @@ zipcode char(5)
 );
 
 
-INSERT INTO `address`(`id`, `address`, `city`, `state`, `zipcode`) VALUES ('1','8042 Pleasant Point Lane','Myrtle Beach','SC','29579’);
+INSERT INTO `address`(`id`, `address`, `city`, `state`, `zipcode`) VALUES ('1','8042 Pleasant Point Lane','Myrtle Beach','SC','29579');
+
+
+CREATE TABLE tableC(
+number varchar(13)
+);
+
+INSERT INTO `tableC`(`number`) VALUES (843-555-0987)
+ );
